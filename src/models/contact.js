02ac929c1085eb6887160bbc02a481d8ed111d79
Sumedh Nimkarde <blesssumedh@gmail.com>
2019-05-19 17:39:41 +0530

    :recycle: Refactor the contact model to use timestamps

--- src/models/contact.js
+++ src/models/contact.js
@@ -6,24 +6,27 @@ const Schema = mongoose.Schema;
 const emailSchema = require("./email");
 const phoneSchema = require("./phone");
 
-const contactSchema = Schema({
-  name: {
-    type: String,
-    required: true
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: [
+      {
+        type: emailSchema,
+        required: false
+      }
+    ],
+    phone: [
+      {
+        type: phoneSchema,
+        required: false
+      }
+    ]
   },
-  email: [
-    {
-      type: emailSchema,
-      required: false
-    }
-  ],
-  phone: [
-    {
-      type: phoneSchema,
-      required: false
-    }
-  ]
-});
+  { timestamps: { createdAt: "createdAt", updatedAt: "createdAt" } }
+);
 
 const Contact = mongoose.model("Contact", contactSchema);
 module.exports = Contact;
--- src/models/group.js
+++ src/models/group.js
@@ -4,19 +4,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Contact = require("./contact");
 
-const groupSchema = Schema({
-  name: {
-    type: String,
-    required: true
+const groupSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    contacts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Contact,
+        required: false
+      }
+    ]
   },
-  contacts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: Contact,
-      required: false
-    }
-  ]
-});
+  { timestamps: { createdAt: "createdAt", updatedAt: "createdAt" } }
+);
 
 const Group = mongoose.model("Group", groupSchema);
 module.exports = Group;
